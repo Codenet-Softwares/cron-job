d66ed94f01be556e789e043f32daba91eedb6fe7
sumancodenet <sumansarkar.codenet8080@gmail.com>
2025-06-15 19:54:01 +0530

    Refactor server.js for improved readability and maintainability; adjust cron schedule for deleteMessage function to run every 12 hours

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-import http from 'http';
-import dotenv from 'dotenv';
-import { sql } from './config/db.js';
-import { updateLottery } from './Job Scheduler/lottery.cron.js';
-import { updateColorGame } from './Job Scheduler/color.cron.js';
-import { deleteMessage } from './Job Scheduler/delete-message.js';
-import cron from 'node-cron';
+import http from "http";
+import dotenv from "dotenv";
+import { sql } from "./config/db.js";
+import { updateLottery } from "./Job Scheduler/lottery.cron.js";
+import { updateColorGame } from "./Job Scheduler/color.cron.js";
+import { deleteMessage } from "./Job Scheduler/delete-message.js";
+import cron from "node-cron";
 
-const envPath = process.env.NODE_ENV === 'production' ? '.env.production' : '.env';
+const envPath =
+  process.env.NODE_ENV === "production" ? ".env.production" : ".env";
 
-dotenv.config({ path: envPath });     
-console.log('Running in environment:', process.env.NODE_ENV);
+dotenv.config({ path: envPath });
+console.log("Running in environment:", process.env.NODE_ENV);
 
-console.log('Running in environment:', process.env.NODE_ENV);
-
-
+console.log("Running in environment:", process.env.NODE_ENV);
 
 const app = http.createServer();
 
@@ -33,19 +32,19 @@ app.listen(process.env.PORT, () => {
 
   runJob();
 
-  cron.schedule("0 0 */2 * *", () => {
+  cron.schedule("0 */12 * * *", () => {
     console.log("Running deleteMessage at", new Date().toISOString());
     deleteMessage().catch((err) => console.error("Scheduled job error:", err));
   });
 });
 
- process.on('SIGINT', async () => {
-  console.log('\nShutting down server...');
+process.on("SIGINT", async () => {
+  console.log("\nShutting down server...");
   try {
-    await sql.end(); 
-    console.log('Database connection closed.');
+    await sql.end();
+    console.log("Database connection closed.");
   } catch (error) {
-    console.error('Error closing DB connection:', error);
+    console.error("Error closing DB connection:", error);
   }
   process.exit(0);
 });
